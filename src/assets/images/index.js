7af22968f58de161c23190f5da6217acc5206a91
raphael <you@example.com>
2024-02-22 02:03:08 +0100

    first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,16 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+import job from './job.svg'
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo,
+    job
+}
---
+++ src/constants/index.js
@@ -0,0 +1,197 @@
+import { job } from "../assets/images";
+import {
+  car,
+  contact,
+  css,
+  estate,
+  vite,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+  firebase,
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: firebase,
+    name: "Firebase",
+    type: "Backend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: vite,
+    name: "Vite",
+    type: "Frontend",
+  },
+];
+
+export const experiences = [
+  {
+    title: "UpComing",
+    company_name: "Pending",
+    icon: job,
+    iconBg: "#accbe1",
+    date: "Month 0000 - Month 0000",
+    points: ["null", "null", "null", "null"],
+  },
+  {
+    title: "UpComing",
+    company_name: "Pending",
+    icon: job,
+    iconBg: "#fbc3bc",
+    date: "Month 0000 - Month 0000",
+    points: ["null", "null", "null", "null"],
+  },
+  {
+    title: "Upcoming",
+    company_name: "Pending",
+    icon: job,
+    iconBg: "#b7e4c7",
+    date: "Month 0000 - Month 0000",
+    points: ["null", "null", "null", "null"],
+  },
+  {
+    title: "Upcoming",
+    company_name: "Pending",
+    icon: job,
+    iconBg: "#a2d2ff",
+    date: "Month 0000 - Month 0000",
+    points: ["null", "null", "null", "null"],
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/YourGitHubUsername",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/YourLinkedInUsername",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "Upcoming",
+    description:
+      "Null",
+    link: "https://github.com/onunkwor",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Upcoming",
+    description:
+      "Null",
+    link: "https://github.com/onunkwor",
+  },
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "Upcoming",
+    description:
+    "Null",
+  link: "https://github.com/onunkwor",
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "Upcoming",
+    description:
+    "Null",
+  link: "https://github.com/onunkwor",
+  },
+  {
+    iconUrl: estate,
+    theme: "btn-back-black",
+    name: "Upcoming",
+    description:
+      "Null",
+    link: "https://github.com/onunkwor",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "Upcoming",
+    description:
+      "Null",
+    link: "https://github.com/onunkwor",
+  }
+];
