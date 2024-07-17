a917c7f556bfd1297a5530f23cf390e3038b5326
raphael <you@example.com>
2024-07-17 14:06:04 +0100

    Add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar";
 import Home from "../src/pages/Home";
 import About from "../src/pages/About";
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
+      <Analytics />
     </main>
   );
 };
